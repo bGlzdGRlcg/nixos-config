a62d526fa7ec0796b0ee7fa5689319370e471112
bGlzdGRlcg <listder@listder.xyz>
2026-02-14 20:21:18 +0800

    chore: add listder to aria2 group for directory access

--- home/users.nix
+++ home/users.nix
@@ -8,6 +8,7 @@
       "wheel"
       "networkmanager"
       "podman"
+      "aria2"
     ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJYQjUG/QYQRZIn9lGSh1iwvJcBwNZZuiKG84ls5J2WWpypkSbXsEUV84ZqJSELbCpD7LtvTFeatluQjI1x2LNOEknsT+tmeoW0QOZCKUrZ2t4vkwnOQTVXtg2SBxm0JypsgTxHQel0/R9RPkZs9jEAfrIBNkwZ9mbj2CYwGMG8R6gjCKDmEzNiUFq46ioddDWel1LVTHc2isXZ3N225LMKRoydHMpo5t3idEhBNoBkK+pB+BFUkqpInC39u4QFgImJxmoW/aRzjR8PKb1t9nGlC525k7+IQHjcHhRx5KvSmEQwfMjqqOksSIqfPMYjZKlluhQfrpnyL6MvQDpYXxx listder@listder"
