a39537b2b7512a395d2f12d61a3f427160d7807b
bGlzdGRlcg <listder@listder.xyz>
2026-06-29 15:53:39 +0800

    chore: add xwininfo, yad, scummvm, and inotify-tools to environment

--- system/environment.nix
+++ system/environment.nix
@@ -52,6 +52,10 @@
       OVMF
       edk2
       podman-compose
+      xwininfo
+      yad
+      scummvm
+      inotify-tools
       (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
         qemu-system-x86_64 \
         -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
